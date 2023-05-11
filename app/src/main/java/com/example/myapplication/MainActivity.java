ab881f9ce7a5a4d1e98b3f5a308714be8054cf62
scx <scx@126.com>
2023-05-11 18:28:45 +0800

    提交代码

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView)findViewById(R.id.button_0);
+        String s = textView.getText().toString();
+        System.out.println("s=="+s);
+    }
+}
